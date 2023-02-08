545fc0848ab422632be135af08d838d4913f5375
mbenazea <mbenazea@gmail.com>
2023-02-07 19:03:50 -0500

    modified file.sh script

--- file.sh
+++ file.sh
@@ -1,18 +1,19 @@
 #!/bin/bash
 
 
-    #Author : Utrains
-    #Date : 01-Nov-2021
+    #Author : Marcelus B.
+    #Date : 01-Nov-2022
     
 ## ---------- If statement and file   -----------------
 
 # -----  check if the file exist with the filename ------------
 
-FILE-NAME=linux
-if [ -f ${FILE-NAME} ]   
-    then 
-    echo "file exist"
+# take input from user:
+read -p "Enter the name of the file: " FILE_NAME
+if [ -f ${FILE_NAME} ]; then 
+    echo "the file exists"
+    echo " the path is: "
+    pwd ${FILE_NAME}
  else 
- touch success   
+    echo "The file does not exist or is not a rugular file" 
 fi
-
