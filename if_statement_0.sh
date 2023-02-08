8be2e3a50eaa4144b498f04b52850212ce69d559
mbenazea <mbenazea@gmail.com>
2023-02-07 21:42:16 -0500

    if statement modified

--- if_statement_0.sh
+++ if_statement_0.sh
@@ -1,9 +1,16 @@
 #!/bin/bash
 
-    #Author : Utrains
+    #Author : Marcelus Benazea
     #Date : 01-Nov-2021
 
 ## ---------- If statement allow you to check specific condition  -----------------
+#  != (not equal to)
+#  -eq (equal to)
+#  -ne (not equal to)
+#  -gt (greater than)
+#  -ge (greater than or equal to)
+#  -lt (less than)
+#  -le (les than or equal to)
 
 if [ 50 -eq 100 ]   # --> this line check if the numer 50 is equal to the number 100
 then 
